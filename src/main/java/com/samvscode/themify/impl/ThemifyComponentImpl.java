52ab1d3378dc2c87f3484e00bb653eaea4490603
Sanyam Singh <ssingh18@atlassian.com>
2024-05-07 02:48:24 +0530

    Rename files, folders and classes. Remove test folder and delete default resources for clean slate to build plugin upon.

---
+++ src/main/java/com/samvscode/themify/impl/ThemifyComponentImpl.java
@@ -0,0 +1,20 @@
+package com.samvscode.themify.impl;
+
+import com.atlassian.sal.api.ApplicationProperties;
+import com.samvscode.themify.api.ThemifyComponent;
+
+public class ThemifyComponentImpl implements ThemifyComponent {
+    private final ApplicationProperties applicationProperties;
+
+    public ThemifyComponentImpl(final ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    public String getName() {
+        if (null != applicationProperties) {
+            return "themifyComponent:" + applicationProperties.getDisplayName();
+        }
+
+        return "themifyComponent";
+    }
+}
